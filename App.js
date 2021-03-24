886c7533778db0e00b25c5e070a5e9f14acf8c20
AigaGoto <s1811337@u.tsukuba.ac.jp>
2021-03-24 09:45:42 +0900

    init

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+// import {Buttons} from './components/Buttons'
 
-export default function App() {
+const App = () => {
+
+  const [num, setNum] = useState(0)
+  const [time, setTime] = useState(0)
+  const [isWork, setIsWork] = useState(false)
+
+  let startTime = 0
+  let stockTime = 0
+  let stopTime = 0
+  
+  const startWatch = () => {
+    startTime = performance.now()
+    setIsWork(true)
+  }
+  const stopWatch = () => {
+    setIsWork(false)
+    stockTime = time
+  }
+  const resetWatch = () => {
+    startTime = 0
+    stopTime = 0
+    setTime(0)
+  }
+
+  useEffect(() => {
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{num}</Text>
+      <TouchableOpacity style={styles.btn} onPress={() => startWatch()}>
+        <Text style={styles.start}>スタート</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btn} onPress={() => stopWatch()}>
+        <Text style={styles.end}>ストップ</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btn} onPress={() => resetWatch()}>
+        <Text style={styles.reset}>リセット</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btn: {
+    padding: 25,
+    backgroundColor: 'black',
+    color: 'white',
+    borderRadius: 50,
+  },
+  start: {
+    color: 'green'
+  },
+  end: {
+    color: 'red'
+  },
+  reset: {
+    color: 'white'
+  }
 });
+
+export default App
---
+++ components/Buttons.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { Button } from 'react-native';
+
+const Buttons = () => {
+    return (
+        <Button title="Learn More"/>
+    )
+}
+
+export default Buttons
