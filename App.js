d076f6f622adcbf9151f6fe4fea280a6245f409b
AigaGoto <s1811337@u.tsukuba.ac.jp>
2021-03-25 18:01:42 +0900

    ボタンの配置を変更しました

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-// import {Buttons} from './components/Buttons'
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 const App = () => {
 
@@ -11,59 +10,144 @@ const App = () => {
   const startWatch = () => {
     const startTime = new Date().getTime()
     const id = setInterval(() => {
-      setTime(new Date().getTime() - startTime)
-      // console.log(new Date().getTime())
+      setTime(new Date().getTime() - startTime + time)
     }, 10)
     setIntervalId(id)
   }
+
   const stopWatch = () => {
-    console.log("OK")
     clearInterval(intervalId)
+    setIntervalId(null)
   }
+
   const resetWatch = () => {
     setTime(0)
   }
 
+  const formatDisplay = (number) => {
+    // 一桁の場合は、前に0をつける
+    // 返却は文字列型になる
+    if (number < 10) {
+      number = '0' + number
+    }
+    return number
+  }
+
   return (
     <View style={styles.container}>
-      <Text>{time}</Text>
-      <TouchableOpacity style={styles.btn} onPress={() => startWatch()}>
-        <Text style={styles.start}>スタート</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.btn} onPress={() => stopWatch()}>
-        <Text style={styles.end}>ストップ</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.btn} onPress={() => resetWatch()}>
-        <Text style={styles.reset}>リセット</Text>
-      </TouchableOpacity>
+      <View style={displays.displayContainer}>
+        <Text style={displays.timer}>{formatDisplay(Math.floor(time / (60 * 1000)))}</Text>
+        <Text style={displays.colon}>:</Text>
+        <Text style={displays.timer}>{formatDisplay(Math.floor(time / (1000)) % 60)}</Text>
+        <Text style={displays.colon}>:</Text>
+        <Text style={displays.timer}>{formatDisplay(Math.floor((time % 1000) / 10))}</Text>
+      </View>
+      <View style={{position: 'relative'}}>
+      {time === 0 && (
+        <TouchableOpacity style={buttons.startBtn} onPress={() => startWatch()}>
+          <Text style={btnText.start}>Start</Text>
+        </TouchableOpacity>
+      )}
+      {time > 0 && intervalId && (
+        <TouchableOpacity style={buttons.stopBtn} onPress={() => stopWatch()}>
+            <Text style={btnText.stop}>Stop</Text>
+          </TouchableOpacity>
+      )}
+      {time > 0 && !intervalId && (
+        <View style={{flexDirection: 'row'}}> 
+          <TouchableOpacity style={buttons.resetBtn} onPress={() => resetWatch()}>
+            <Text style={btnText.reset}>Reset</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={buttons.startBtn} onPress={() => startWatch()}>
+            <Text style={btnText.start}>Start</Text>
+          </TouchableOpacity>          
+        </View>
+      )}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const btnBase = {
+  position: 'absolute',
+  width: 70,
+  height: 70,
+  backgroundColor: 'rgba(0,0,0,0.7)',
+  borderRadius: 70,
+  margin: 4,
+  textAlign: 'center',
+  justifyContent: 'center'
+}
+
+const buttons = StyleSheet.create({
+  startBtn: {
+    ...btnBase,
+    left: 96,
+  },
+  stopBtn: {
+    ...btnBase,
+    left: 96,
+  },
+  resetBtn: {
+    ...btnBase,
+    right: 96,
+  },
+})
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
   btn: {
-    padding: 25,
-    backgroundColor: 'black',
-    color: 'white',
-    borderRadius: 50,
+    position: 'absolute',
+    left: 96,
+    width: 70,
+    height: 70,
+    backgroundColor: 'rgba(0,0,0,0.7)',
+    borderRadius: 70,
+    margin: 4,
+    textAlign: 'center',
+    justifyContent: 'center'
   },
-  start: {
-    color: 'green'
+});
+
+const displays = StyleSheet.create({
+  displayContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
-  end: {
-    color: 'red'
+  timer: {
+    width: 100,
+    fontSize: 80
   },
-  reset: {
-    color: 'white'
+  colon: {
+    width: 20,
+    fontSize: 80
   }
 });
+
+const btnTextBase = {
+  fontSize: 18,
+  fontWeight: '500',
+}
+
+const btnText = StyleSheet.create({
+  start: {
+    ...btnTextBase,
+    color: '#1AFF1A',
+  },
+  stop: {
+    ...btnTextBase,
+    color: '#FF1A40',
+  },
+  reset: {
+    ...btnTextBase,
+    color: 'white',
+  }
+})
 
 export default App
